1e328abde00584439eb8728d8300c152e0c68ab9
angelaczr <angelacaezaria@gmail.com>
2023-12-04 20:13:45 +0700

    new index

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
-export * from "./kecil"
-export * from "./besar"
+import Button from './button' 
+import Input from './input'
+import CategoryTab from './categoryTab'
+import Pilihan from './pilihan'
+
+export {Button, Input, CategoryTab, Pilihan}
